784f8cdf712b0f7f9a5e88f1b90d8ca59032df41
apoorvSInha <apoorvsinha96@gmail.com>
2023-03-24 09:13:13 +0530

    some changes in home

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -6,7 +6,7 @@ import org.testng.annotations.Test;
 import base.TestBase;
 
 public class HomePage extends TestBase{
-	
+	@Test
 	public void clickOptions() {
 		click("hamburg_home_page_XPATH");
 		click("login_button_XPATH");
